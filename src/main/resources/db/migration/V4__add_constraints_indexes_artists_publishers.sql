7fd4f87aa0ce1153182a08e7f57361984547265a
KhangHuynh <khanghqse182958@fpt.edu.vn>
2025-10-10 13:22:56 +0700

    refactor: change database

--- src/main/resources/db/migration/V4__add_constraints_indexes_artists_publishers.sql
+++ src/main/resources/db/migration/V4__add_constraints_indexes_artists_publishers.sql
@@ -1,10 +1,3 @@
--- Unique constraints cho Publishers
-ALTER TABLE publishers
-  ADD CONSTRAINT uq_publishers_publisher_name UNIQUE (publisher_name);
-
--- Unique constraints cho Artists  
-ALTER TABLE artists
-  ADD CONSTRAINT uq_artists_artist_name UNIQUE (artist_name);
 
 -- Indexes cho tra cứu
 CREATE INDEX IF NOT EXISTS idx_publishers_name ON publishers(publisher_name);
