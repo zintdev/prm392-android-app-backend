23356ef892b73cb318dad827a6ff41b0dc71b47f
Tấn Triệu <144522038+zintdev@users.noreply.github.com>
2025-10-10 22:36:24 +0700

    db: reupdate sql file

--- src/main/resources/db/migration/V4__add_constraints_indexes_artists_publishers.sql
+++
@@ -1,5 +0,0 @@
-
--- Indexes cho tra cứu
-CREATE INDEX IF NOT EXISTS idx_publishers_name ON publishers(publisher_name);
-CREATE INDEX IF NOT EXISTS idx_artists_name ON artists(artist_name);
-CREATE INDEX IF NOT EXISTS idx_artists_type ON artists(artist_type);
