a33397633450a14584624bf9d596214373549a0b
Tấn Triệu <144522038+zintdev@users.noreply.github.com>
2025-10-16 23:35:45 +0700

    fix: rename user dto

---
+++ src/main/java/com/example/backend/dto/user/UserResponse.java
@@ -0,0 +1,19 @@
+package com.example.backend.dto.user;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Integer id;
+    private String username;
+    private String email;
+    private String phoneNumber;
+    private String role;
+}
